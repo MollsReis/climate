272ab4b44f1bf72424af1ad636c41ee36d0bb2bb
EvilScott <reis.robert.s@gmail.com>
2017-01-30 23:52:20 -0800

    style station marker and use state line for bounds

--- src/map.js
+++ src/map.js
@@ -9,9 +9,19 @@ class Map extends React.Component {
         let stationsList = require('./stations.json').results.sort((a, b) => { return a.name.localeCompare(b.name); });
         let statesGeoJSON = require('./states.json');
         let stations = L.featureGroup(stationsList.map((station) => {
-            return L.marker([station.latitude, station.longitude], { name: station.name }).bindPopup(station.name);
+            return L.circleMarker([station.latitude, station.longitude], {
+                name: station.name,
+                color: '#333',
+                fill: true,
+                fillColor: '#ff6600',
+                fillOpacity: 1,
+                opacity: 1,
+                radius: 6,
+                weight: 2
+            }).bindPopup(station.name);
         }));
         let stateBoundaries = L.geoJson(statesGeoJSON, {
+            fill: false,
             filter: (feature) => { return feature.properties.NAME === 'Washington'; }
         });
         this.state = { stations: stations, stateBoundaries: stateBoundaries };
@@ -21,7 +31,7 @@ class Map extends React.Component {
         const tileUrl = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
         const attribution = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, ' +
             'Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
-        let featureBounds = this.state.stations.getBounds();
+        let bounds = this.state.stateBoundaries.getBounds();
         let attributionControl = L.control.attribution({ position: 'bottomleft' }).addAttribution(attribution);
         this.map = L.map('map', {
             attributionControl: false,
@@ -30,10 +40,10 @@ class Map extends React.Component {
                 this.state.stations,
                 this.state.stateBoundaries
             ],
-            maxBounds: featureBounds,
+            maxBounds: bounds,
             minZoom: 7,
             zoomControl: false
-        }).fitBounds(featureBounds).addControl(attributionControl);
+        }).fitBounds(bounds).addControl(attributionControl);
     }
 
     handleListItemClick(station) {
